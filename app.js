e02c0b7522a0b9d10b0542c28067c5e0c4bff3b6
Rizwan114 <160789107+Rizwan114@users.noreply.github.com>
2024-03-10 02:20:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+////// Question No.3  ///////
+let message = "Rizwan Ali";
+//////////           UpperCase       /////////////
+console.log(message.toUpperCase());
+/////////           LowerCase    //////////////////
+console.log(message.toLowerCase());
+///////            TitleCase      ///////////////
+console.log(message.charAt(0).toUpperCase() + message.slice(1).toLowerCase());
---
+++ app.ts
@@ -0,0 +1,21 @@
+
+
+////// Question No.3  ///////
+
+let message : string = "Rizwan Ali"
+
+
+//////////           UpperCase       /////////////
+
+console.log(message.toUpperCase());
+
+
+/////////           LowerCase    //////////////////
+
+
+console.log(message.toLowerCase());
+
+
+///////            TitleCase      ///////////////
+
+console.log(message.charAt(0).toUpperCase() +  message.slice(1).toLowerCase() );
